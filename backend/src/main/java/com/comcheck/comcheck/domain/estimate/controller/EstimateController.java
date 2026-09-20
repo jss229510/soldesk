6b6ea06433750d2c2b38bbf1319ebda7678cca70
seijam906-del <seijam906@gmail.com>
2026-09-20 21:03:41 +0900

    09-20
    estimate -repository,service,controller 작성중

--- backend/src/main/java/com/comcheck/comcheck/domain/estimate/controller/EstimateController.java
+++ backend/src/main/java/com/comcheck/comcheck/domain/estimate/controller/EstimateController.java
@@ -1,5 +1,28 @@
 package com.comcheck.comcheck.domain.estimate.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+// HTTP 요청을 처리하고 결과를 JSON 형태로 반환하는 Controller
+@RequestMapping("/api")
+// 견적의 API와 User의 API를 한 Controller에서 함께 다루기 위해 api까지만 지정
+
 public class EstimateController {
+
+    // POST /api/estimates
     
+
+
+      // 견적 생성
+
+      // 공개 견적 목록 조회
+
+      // 견적 하나 조회
+
+      // 특정 회원의 견적 목록 조회
+
+      // 견적 수정
+
+      // 견적 삭제
 }
--- backend/src/main/java/com/comcheck/comcheck/domain/estimate/repository/EstimateRepository.java
+++ backend/src/main/java/com/comcheck/comcheck/domain/estimate/repository/EstimateRepository.java
@@ -1,6 +1,8 @@
 package com.comcheck.comcheck.domain.estimate.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.comcheck.comcheck.domain.estimate.entity.Estimate;
@@ -9,4 +11,11 @@ import java.util.List;
 
 public interface EstimateRepository extends JpaRepository<Estimate, Long>  {
     
+
+    // 특정 회원의 견적 목록(최신순)
+    List<Estimate> findByUserIdOrderByCreatedAtDesc(Long userId);
+    
+    // 공개 여부로 견적 조회 (최신순, 페이지 단위)
+    Page<Estimate> findBySharedOrderByCreatedAtDesc(boolean shared, Pageable pageable);
+
 }
